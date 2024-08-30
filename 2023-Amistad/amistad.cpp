2485c2bb8488799d10cadf6c82cd5ca4fd9d734f
Estudiantes <sodocamilo@gmail.com>
2024-08-30 19:22:06 -0300

    Amistad pt1

---
+++ 2023-Amistad/amistad.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    char* str = argv[1];
+    char largo[7];
+    int i = 0;
+    for (; *str != '\n'; i++) 
+        largo[i] = str[i]
+    char esperado
+    
+
+
+    return 0;
+}
+
+int largo(char str[]) {
+
+}
