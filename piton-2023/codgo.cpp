fc61eae728ea6852a63ee84caea1b73c12cb03db
CamiCamiCami <sodocamilo@gmail.com>
2024-10-04 21:33:21 -0300

    mejor formula

--- piton-2023/codgo.cpp
+++ piton-2023/codgo.cpp
@@ -5,7 +5,7 @@
 int main(void) {
     double largo;
     scanf("%lf", &largo);
-    double resultado = (largo*largo) + 0.5*largo*(largo+1)*(largo-1-(2*largo + 1) / 3);
+    double resultado = ((largo*largo*largo)/6) + ((largo*largo)/2) - ((2*largo)/3);
     resultado = 2 * resultado + largo;
     printf("%i\n", (int)resultado);
 }
